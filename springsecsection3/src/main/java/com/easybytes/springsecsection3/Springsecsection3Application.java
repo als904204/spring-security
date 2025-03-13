30c466138d1ca87b78ad4ff19c4fef983d03d20c
Min <als904204@naver.com>
2025-03-13 21:41:32 +0900

    비밀번호 유출 여부 검증하는 Bean 등록

--- springsecsection3/src/main/java/com/easybytes/springsecsection3/Springsecsection3Application.java
+++ springsecsection3/src/main/java/com/easybytes/springsecsection3/Springsecsection3Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springsecsection3Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Springsecsection3Application.class, args);
 	}
+
 
 }
