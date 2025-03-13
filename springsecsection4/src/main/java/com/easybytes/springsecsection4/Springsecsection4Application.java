a851ceb3abf2671a9774c81447374114135e52d7
Min <als904204@naver.com>
2025-03-13 23:55:47 +0900

    add comment

--- springsecsection4/src/main/java/com/easybytes/springsecsection4/Springsecsection4Application.java
+++ springsecsection4/src/main/java/com/easybytes/springsecsection4/Springsecsection4Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableWebSecurity -> SpringBoot라면 자동으로 활성화 됨.
 public class Springsecsection4Application {
 
 
--- springsecsection4/src/main/resources/sql/script.sql
+++ springsecsection4/src/main/resources/sql/script.sql
@@ -1,3 +1,4 @@
+# 시큐리티 공식 문서 기본 테이블
 create table users(username varchar(50) not null primary key,password varchar(500) not null,enabled boolean not null);
 create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username, authority);
@@ -12,6 +13,7 @@ INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$88.f6upbBvy0okEa7OfH
 INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
 
 
+# PK가 있는 회원 테이블
 CREATE TABLE `customer` (
                             `id` int NOT NULL AUTO_INCREMENT,
                             `email` varchar(45) NOT NULL,
@@ -24,4 +26,4 @@ CREATE TABLE `customer` (
 INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('happy@example.com', '{noop}EazyBytes@12345', 'read');
 INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m', 'admin');
 
-select * from users
+select * from customer
