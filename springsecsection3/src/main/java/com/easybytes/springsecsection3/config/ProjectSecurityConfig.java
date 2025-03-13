7d220ee46f171c5089d785b9d29efb1f6bc52440
Min <als904204@naver.com>
2025-03-13 21:35:38 +0900

    InMemory User, Admin 구성

--- springsecsection3/src/main/java/com/easybytes/springsecsection3/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/easybytes/springsecsection3/config/ProjectSecurityConfig.java
@@ -4,8 +4,15 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,10 +27,29 @@ public class ProjectSecurityConfig {
             .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
             .requestMatchers("/notices", "/contact", "/error").permitAll());
 
-        http.formLogin(AbstractHttpConfigurer::disable);
-        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{bcrypt}$2a$12$PI/9yeK1u9xmmVEG1RQyzupCJeHu7zattKuwzrYJvgm.I2wWoWWUe").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$PI/9yeK1u9xmmVEG1RQyzupCJeHu7zattKuwzrYJvgm.I2wWoWWUe").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // return new BCryptPasswordEncoder() 안하는 이유
+        // Spring Security 가 언젠간 권장 사항을 BCryptPasswordEncoder() 말고 다른 걸로 변경할수도 있음
+        // 아래 팩토리는 시큐리티가 권장하는걸 기본값으로 유연성 제공 함
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+
 
 }
