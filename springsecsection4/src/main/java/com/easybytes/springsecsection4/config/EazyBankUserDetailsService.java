45ba677f075f7c59e7292534c94f8c57409015bf
Min <als904204@naver.com>
2025-03-13 23:55:57 +0900

    add CustomUserDetailsService

---
+++ springsecsection4/src/main/java/com/easybytes/springsecsection4/config/EazyBankUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.easybytes.springsecsection4.config;
+
+import com.easybytes.springsecsection4.model.Customer;
+import com.easybytes.springsecsection4.repository.CustomerRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class EazyBankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found" + username));
+        List<GrantedAuthority> authorities = List.of(
+            new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
